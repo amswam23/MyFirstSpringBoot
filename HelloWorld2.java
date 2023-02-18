e84bbff3a78e8e46b11a3c2345aaf792c25bb3f2
swami <amswam23@gmail.com>
2023-02-17 22:27:47 -0500

    Added new HelloWorld2.java

---
+++ HelloWorld2.java
@@ -0,0 +1,15 @@
+class HelloWorld2 {
+
+   public static void main ( String[] args ){
+      System.out.println("Step1");
+      int x=10;
+      System.out.println(x);
+      System.out.println("new line");
+      System.out.println("second new line");
+      System.out.println("Third new line");
+      System.out.println("fourth line new line");
+
+   System.out.println("HelloWorld");
+}
+
+}
